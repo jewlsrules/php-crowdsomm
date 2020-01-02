fda7a60976dd7f9721f1c4b1c3b95092d8d961be
Jewls Krueger <Juliarkrueger@gmail.com>
2020-01-02 11:37:55 -0800

    set up review paths

---
+++ api/controllers/reviews.php
@@ -0,0 +1,24 @@
+<?php
+include_once __DIR__ . '/../models/review.php';
+header('Content-Type: application/json');
+
+if ($_REQUEST['action'] == 'index') {
+  echo json_encode(Reviews::all());
+} elseif ($_REQUEST['action'] == 'post') {
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $new_review = new Review(null, $body_object->user_id, $body_object->restaurant_id, $body_object->dish_name, $body_object->dish_id, $body_object->stars, $body_object->review_text);
+  $all_reviews = Reviews::create($new_review);
+  echo json_encode($all_reviews);
+} elseif ($_REQUEST['action'] == 'update'){
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $updated_review = new Review($_REQUEST['id'], $body_object->user_id, $body_object->restaurant_id, $body_object->dish_name, $body_object->dish_id, $body_object->stars, $body_object->review_text);
+  $all_reviews = Reviews::update($updated_review);
+  echo json_encode($all_reviews);
+} elseif ($_REQUEST['action'] == 'delete') {
+  $all_reviews = Reviews::delete($_REQUEST['id']);
+  echo json_encode($all_reviews);
+}
+
+?>
--- api/models/review.php
+++ api/models/review.php
@@ -38,4 +38,51 @@ class Review {
   }
 }
 
+// this is the "factory" that all of the functions will go through
+class Reviews {
+  //get all applications function
+  static function all(){
+    $reviews = array();
+
+    $results = pg_query("SELECT * FROM applications"); //getting the information from the database for all applications
+    $row_object = pg_fetch_object($results);
+
+    while($row_object){ //while there's a result object...
+      $new_review = new Review(
+        intval($row_object->id),
+        intval($row_object->user_id),
+        $row_object->restaurant_id,
+        $row_object->dish_name,
+        intval($row_object->dish_id),
+        intval($row_object->stars),
+        $row_object->review_text
+      );
+      $reviews[] = $new_review;
+      $row_object = pg_fetch_object($results);
+    }
+    return $reviews;
+  }
+
+  static function create($review){
+    $query ="INSERT INTO reviews (user_id, restaurant_id, dish_name, dish_id, stars, review_text) VALUES ($1, $2, $3, $4, $5, $6)";
+    $query_params = array($review->user_id, $review->restaurant_id, $review->dish_name, $review->dish_id, $review->stars, $review->review_text);
+    pg_query_params($query, $query_params);
+    return self::all();
+  }
+
+  static function update($updated_review){
+    $query = "UPDATE reviews SET user_id = $1, restaurant_id = $2, dish_name = $3, dish_id = $4, stars = $5, review_text = $6 WHERE id = $7";
+    $query_params = array($updated_review->user_id, $updated_review->restaurant_id, $updated_review->dish_name, $updated_review->dish_id, $updated_review->stars, $updated_review->review_text, $updated_review->id);
+    $result = pg_query_params($query, $query_params);
+    return self::all();
+  }
+
+  static function delete($id){
+    $query = "DELETE FROM reviews WHERE id = $1";
+    $query_params = array($id);
+    $result = pg_query_params($query, $query_params);
+    return self::all();
+  }
+}
+
 ?>
