e8065969602a0f1f7eec806400cd9e8211f6f400
Jewls Krueger <Juliarkrueger@gmail.com>
2020-01-06 11:39:23 -0800

    troubleshooting post route for dishes

--- api/controllers/dishes.php
+++ api/controllers/dishes.php
@@ -8,8 +8,8 @@ if ($_REQUEST['action'] == 'index') {
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
   $new_dish = new Dish(null, $body_object->dish_name, $body_object->restaurant_id);
-  $all_reviews = Dishes::create($new_dish);
-  echo json_encode($all_reviews);
+  $all_dishes = Dishes::create($new_dish);
+  echo json_encode($all_dishes);
 } elseif ($_REQUEST['action'] == 'update'){
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
