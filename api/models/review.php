db5e383050a0abca4b631cb49a8f831ff5572353
Jewls Krueger <Juliarkrueger@gmail.com>
2020-01-06 18:35:19 -0800

    fixed sort by on average

--- api/models/review.php
+++ api/models/review.php
@@ -127,7 +127,7 @@ class Reviews {
 
   static function getAverageStarsByDish($id){
 
-    $query = "SELECT dish_name, dish_id, AVG(stars) AS stars FROM reviews WHERE dish_id = $1 GROUP BY dish_id, dish_name SORT BY stars DESC";
+    $query = "SELECT dish_name, dish_id, AVG(stars) AS stars FROM reviews WHERE dish_id = $1 GROUP BY dish_id, dish_name";
     $query_params = array($id);
 
     $results = pg_query_params($query, $query_params);
